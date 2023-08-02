97773087cb6e77f26000bcff354ff9549cc7b498
044860 <044860@UNI-LAB5-123>
2023-08-02 20:13:35 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Controllers/TesteController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models.Request;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TesteController : Controller
+    {
+        [HttpGet]
+        public IActionResult Post(AlunoViewModel alunoViewModel)
+        {
+            if(alunoViewModel.Idade < 0)
+            {
+                return BadRequest("Idade invalida");
+            }
+           return Ok("Aluno criado com sucesso");
+        }
+    }
+}
---
+++ WebApplication1/Models/Request/AlunoViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models.Request
+{
+    public class AlunoViewModel
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+    }
+}
