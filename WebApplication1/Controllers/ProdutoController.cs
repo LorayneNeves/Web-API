5f0256e42fb9ddf8172e74683c27760d1d967fb2
044860 <lorayne3.8neves@outlook.com>
2023-08-16 20:13:31 -0300

    update

--- WebApplication1/Controllers/ProdutoController.cs
+++ WebApplication1/Controllers/ProdutoController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 using WebApplication1.Models.Request;
 
 namespace WebApplication1.Controllers
@@ -7,22 +9,92 @@ namespace WebApplication1.Controllers
     [Route("[controller]")]
     public class ProdutoController : ControllerBase
     {
-        [HttpGet("{codigo")]
+        private readonly string _produtoCaminhoArquivo;
+
+        public ProdutoController()
+        {
+            _produtoCaminhoArquivo = Path.Combine(Directory.GetCurrentDirectory(), "Data", "produto.json");
+        }
+
+        #region Métodos arquivo
+        private List<ProdutoViewModel> LerProdutosDoArquivo()
+        {
+            if (!System.IO.File.Exists(_produtoCaminhoArquivo))
+            {
+                return new List<ProdutoViewModel>();
+            }
+
+            string json = System.IO.File.ReadAllText(_produtoCaminhoArquivo);
+
+            if (string.IsNullOrEmpty(json))
+            {
+                return new List<ProdutoViewModel>();
+            }
+
+            return JsonConvert.DeserializeObject<List<ProdutoViewModel>>(json);
+        }
+
+        private int ObterProximoCodigoDisponivel()
+        {
+            List<ProdutoViewModel> produtos = LerProdutosDoArquivo();
+            if (produtos.Any())
+            {
+                return produtos.Max(p => p.Codigo) + 1;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        private void EscreverProdutosNoArquivo(List<ProdutoViewModel> produtos)
+        {
+            string json = JsonConvert.SerializeObject(produtos);
+            System.IO.File.WriteAllText(_produtoCaminhoArquivo, json);
+        }
+        #endregion
+
+        #region Operações CRUD
+        [HttpGet("{codigo}")]
        public IActionResult Get(int codigo)
         {
-            return Ok();
+            List < ProdutoViewModel> listaTodosProdutos = LerProdutosDoArquivo();
+            var produtoProcurado = listaTodosProdutos.Where(p => p.Codigo == codigo);
+            if (!produtoProcurado.Any()) return NotFound();
+            
+            return Ok(produtoProcurado.First());
         }
 
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok();
+            List<ProdutoViewModel> listaTodosProdutos = LerProdutosDoArquivo();
+            return Ok(listaTodosProdutos);
         }
 
         [HttpPost]
         public IActionResult Post(NovoProdutoViewModel novoProdutoViewModel)
         {
-            return Ok();
+            if (novoProdutoViewModel == null)
+            {
+                return BadRequest();
+            }
+            List<ProdutoViewModel> produtos = LerProdutosDoArquivo();
+            int proximoCodigo = ObterProximoCodigoDisponivel();
+
+            ProdutoViewModel novoProduto = new ProdutoViewModel()
+            {
+                Codigo = proximoCodigo,
+                Descricao = novoProdutoViewModel.Descricao,
+                Preco = novoProdutoViewModel.Preco,
+                Estoque = novoProdutoViewModel.Estoque,
+                Ativo = novoProdutoViewModel.Ativo
+            };
+
+            produtos.Add(novoProduto);
+            EscreverProdutosNoArquivo(produtos);
+
+            return CreatedAtAction(nameof(Get), new { codigo = novoProduto.Codigo }, novoProduto);
 
         }
 
@@ -37,5 +109,6 @@ namespace WebApplication1.Controllers
         {
             return Ok();
         }
+        #endregion
     }
 }
---
+++ WebApplication1/Models/Request/ProdutoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models.Request
+{
+    public class ProdutoViewModel
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public int Estoque { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
